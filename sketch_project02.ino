b4bf7a4837b7566a8b6300b8cb25048cb960d78e
efadzli <efadzli@gmail.com>
2023-08-20 09:42:41 +0800

    Update sketch_project02.ino

--- sketch_project02.ino
+++ sketch_project02.ino
@@ -6,12 +6,15 @@ This code can be downloaded from https://github.com/efadzli/arduino_beginners
 
 void setup() {
   pinMode(9, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(13, HIGH);
   tone(9, 262);
   delay(500);
 
+  digitalWrite(13, LOW);
   tone(9, 494);  
   delay(500);
 }
