c8ff1733636ba48ed272416b2d9804cc010f5415
efadzli <efadzli@gmail.com>
2023-08-28 12:45:11 +0800

    Create sketch_project08_fun.ino

---
+++ sketch_project08_fun.ino
@@ -0,0 +1,40 @@
+/*
+Arduino Training Series by UniKidz
+Robotic Programming Your Engineer
+This code can be downloaded from https://github.com/efadzli/arduino_beginners
+You need to download a library: DHT sensor library by Adafruit (https://github.com/adafruit/DHT-sensor-library)
+*/
+
+#include <DHT.h>
+
+#define DHTPIN 7
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting DHT11 Reading..");
+  pinMode(13, OUTPUT);
+
+  dht.begin();
+}
+
+void loop() {
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print("%  Temperature: ");
+  Serial.print(t);
+  Serial.println("°C ");
+
+  if(t > 25){
+    digitalWrite(13, HIGH);
+  }else{
+    digitalWrite(13, LOW);
+  }
+
+  delay(2000);
+}
