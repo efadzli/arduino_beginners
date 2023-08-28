63ca2e37a95a9e74799f128ff9ecf10216835821
efadzli <efadzli@gmail.com>
2023-08-28 12:01:12 +0800

    Create sketch_project07_fun.ino

---
+++ sketch_project07_fun.ino
@@ -0,0 +1,40 @@
+/*
+Arduino Training Series by UniKidz
+Robotic Programming Your Engineer
+This code can be downloaded from https://github.com/efadzli/arduino_beginners
+*/
+
+const int trigPin = 11;
+const int echoPin = 10;
+
+void setup() { 
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);   
+  pinMode(echoPin, INPUT); 
+  pinMode(buzzerpin, OUTPUT);
+}
+
+void loop() {
+  long duration, inches, cm, mm;
+
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2000); 
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(1000); 
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  cm = duration/29/2;
+
+  Serial.print("The distance is (cm): ");   
+  Serial.println(cm);
+  if(cm < 20){
+    tone(8, 262);
+    delay(150);
+  } if(cm < 10){
+    tone(8, 262);
+    delay(50)
+  }
+  noTone(8);
+  delay(200);
+}
