ba375bbc7c88bb91c8df73d7f2d58bfb225775f8
efadzli <efadzli@gmail.com>
2023-08-28 10:52:37 +0800

    Create sketch_project06_fun.ino

---
+++ sketch_project06_fun.ino
@@ -0,0 +1,29 @@
+/*
+Arduino Training Series by UniKidz
+Robotic Programming Your Engineer
+This code can be downloaded from https://github.com/efadzli/arduino_beginners
+*/
+
+#include <Servo.h>
+Servo myservo;
+int pos = 0;
+int potpin = A0;
+int timer = 0;
+
+void setup() {
+  myservo.attach(13);
+  pinMode(potpin, INPUT);
+}
+
+void loop() {
+  timer = analogRead(potpin);
+  for(pos=0; pos<=180; pos++){
+    myservo.write(pos);
+    delay(timer);
+  }
+
+  for(pos=180; pos>=0; pos--){
+    myservo.write(pos);
+    delay(timer);
+  }
+}
